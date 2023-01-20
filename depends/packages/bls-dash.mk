d69078e4f467962891d1807a83fc5a4a4bc8e5d1
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-01-20 16:00:26 +0000

    36

--- depends/packages/bls-dash.mk
+++ depends/packages/bls-dash.mk
@@ -1,61 +1,42 @@
 package=bls-dash
-$(package)_version=1.2.0
+$(package)_version=1.1.0
 $(package)_download_path=https://github.com/dashpay/bls-signatures/archive
 $(package)_download_file=$($(package)_version).tar.gz
 $(package)_file_name=$(package)-$($(package)_download_file)
 $(package)_build_subdir=build
-$(package)_sha256_hash=94e49f3eaa29bc1f354cd569c00f4f4314d1c8ab4758527c248b67da9686135a
+$(package)_sha256_hash=276c8573104e5f18bb5b9fd3ffd49585dda5ba5f6de2de74759dda8ca5a9deac
 $(package)_dependencies=gmp cmake
-$(package)_patches=0001-Static-sodium.patch
 
-$(package)_relic_version=aecdcae7956f542fbee2392c1f0feb0a8ac41dc5
+$(package)_relic_version=3a23142be0a5510a3aa93cd6c76fc59d3fc732a5
 $(package)_relic_download_path=https://github.com/relic-toolkit/relic/archive
 $(package)_relic_download_file=$($(package)_relic_version).tar.gz
-$(package)_relic_file_name=relic-$($(package)_relic_download_file)
+$(package)_relic_file_name=relic-toolkit-$($(package)_relic_download_file)
 $(package)_relic_build_subdir=relic
-$(package)_relic_sha256_hash=f2de6ebdc9def7077f56c83c8b06f4da5bacc36b709514bd550a92a149e9fa1d
+$(package)_relic_sha256_hash=ddad83b1406985a1e4703bd03bdbab89453aa700c0c99567cf8de51c205e5dde
 
-$(package)_libsodium_version=1.0.18-stable-msvc
-$(package)_libsodium_download_path=https://download.libsodium.org/libsodium/releases
-$(package)_libsodium_download_file=libsodium-$($(package)_libsodium_version).zip
-$(package)_libsodium_file_name=libsodium-$($(package)_libsodium_version).zip
-sspackage)_libsodium_build_subdir=sodium-populate
-$(package)_libsodium_sha256_hash=2b8a6fd060c1ade26f9a66ca71a9602cc5e3cf19c0cdb8c56cd45ace5220fac4
 $(package)_extra_sources=$($(package)_relic_file_name)
-$(package)_extra_sources+=$($(package)_libsodium_file_name)
 
 define $(package)_fetch_cmds
 $(call fetch_file,$(package),$($(package)_download_path),$($(package)_download_file),$($(package)_file_name),$($(package)_sha256_hash)) && \
-$(call fetch_file,$(package),$($(package)_relic_download_path),$($(package)_relic_download_file),$($(package)_relic_file_name),$($(package)_relic_sha256_hash)) && \
-$(call fetch_file,$(package),$($(package)_libsodium_download_path),$($(package)_libsodium_download_file),$($(package)_libsodium_file_name),$($(package)_libsodium_sha256_hash))
+$(call fetch_file,$(package),$($(package)_relic_download_path),$($(package)_relic_download_file),$($(package)_relic_file_name),$($(package)_relic_sha256_hash))
 endef
 
 define $(package)_extract_cmds
   mkdir -p $($(package)_extract_dir) && \
   echo "$($(package)_sha256_hash)  $($(package)_source)" > $($(package)_extract_dir)/.$($(package)_file_name).hash && \
   echo "$($(package)_relic_sha256_hash)  $($(package)_source_dir)/$($(package)_relic_file_name)" >> $($(package)_extract_dir)/.$($(package)_file_name).hash && \
-  echo "$($(package)_libsodium_sha256_hash)  $($(package)_source_dir)/$($(package)_libsodium_file_name)" >> $($(package)_extract_dir)/.$($(package)_file_name).hash && \
   $(build_SHA256SUM) -c $($(package)_extract_dir)/.$($(package)_file_name).hash && \
   tar --strip-components=1 -xf $($(package)_source) -C . && \
-  cp -v $($(package)_source_dir)/$($(package)_libsodium_file_name) . && \
-  cp $($(package)_source_dir)/$($(package)_relic_file_name) . && \
-  ls 
+  cp $($(package)_source_dir)/$($(package)_relic_file_name) .
 endef
 
 define $(package)_set_vars
-  $(package)_config_opts=-DCMAKE_INSTALL_PREFIX=$(host_prefix)
+  $(package)_config_opts=-DCMAKE_INSTALL_PREFIX=$($(package)_staging_dir)/$(host_prefix)
   $(package)_config_opts+= -DCMAKE_PREFIX_PATH=$(host_prefix)
-  $(package)_config_opts+= -DSTLIB=ON -DSHLIB=OFF -DSTBIN=OFF
+  $(package)_config_opts+= -DSTLIB=ON -DSHLIB=OFF -DSTBIN=ON
   $(package)_config_opts+= -DBUILD_BLS_PYTHON_BINDINGS=0 -DBUILD_BLS_TESTS=0 -DBUILD_BLS_BENCHMARKS=0
   $(package)_config_opts_linux=-DOPSYS=LINUX -DCMAKE_SYSTEM_NAME=Linux
   $(package)_config_opts_darwin=-DOPSYS=MACOSX -DCMAKE_SYSTEM_NAME=Darwin
-  ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
-    $(package)_config_opts_darwin+= -DCMAKE_AR=$(host_prefix)/native/bin/$(host)-ar
-    $(package)_config_opts_darwin+= -DCMAKE_RANLIB=$(host_prefix)/native/bin/$(host)-ranlib
-  else
-    $(package)_config_opts_darwin+= -DCMAKE_AR="$($(package)_ar)"
-    $(package)_config_opts_darwin+= -DCMAKE_RANLIB="$($(package)_ranlib)"
-  endif
   $(package)_config_opts_mingw32=-DOPSYS=WINDOWS -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_SHARED_LIBRARY_LINK_C_FLAGS=""
   $(package)_config_opts_i686+= -DWSIZE=32
   $(package)_config_opts_x86_64+= -DWSIZE=64
@@ -63,13 +44,17 @@ define $(package)_set_vars
   $(package)_config_opts_armv7l+= -DWSIZE=32
   $(package)_config_opts_debug=-DDEBUG=ON -DCMAKE_BUILD_TYPE=Debug
 
+  ifneq ($(darwin_native_toolchain),)
+    $(package)_config_opts_darwin+= -DCMAKE_AR="$(host_prefix)/native/bin/$($(package)_ar)"
+    $(package)_config_opts_darwin+= -DCMAKE_RANLIB="$(host_prefix)/native/bin/$($(package)_ranlib)"
+  endif
+
   $(package)_cppflags+=-UBLSALLOC_SODIUM
 endef
 
 define $(package)_preprocess_cmds
-  patch -p1 -i $($(package)_patch_dir)/0001-Static-sodium.patch
-  sed -i.old "s|GIT_REPOSITORY https://github.com/Chia-Network/relic.git|URL \"../../relic-$($(package)_relic_version).tar.gz\"|" CMakeLists.txt && \
-  sed -i.old "s|RELIC_GIT_TAG \".*\"|RELIC_GIT_TAG \"\"|" CMakeLists.txt
+  sed -i.old "s|GIT_REPOSITORY https://github.com/relic-toolkit/relic.git|URL \"../../relic-toolkit-$($(package)_relic_version).tar.gz\"|" src/CMakeLists.txt && \
+  sed -i.old "s|GIT_TAG        .*RELIC_GIT_TAG.*|URL_HASH SHA256=$($(package)_relic_sha256_hash)|" src/CMakeLists.txt
 endef
 
 define $(package)_config_cmds
@@ -86,5 +71,5 @@ define $(package)_build_cmds
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+  $(MAKE) install
 endef
