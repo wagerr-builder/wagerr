a5dec667f9c4d29eeb41d5d861542fddef6432d0
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-01-20 14:36:09 +0000

    31

--- depends/packages/bls-dash.mk
+++ depends/packages/bls-dash.mk
@@ -14,10 +14,10 @@ $(package)_relic_file_name=relic-$($(package)_relic_download_file)
 $(package)_relic_build_subdir=relic
 $(package)_relic_sha256_hash=f2de6ebdc9def7077f56c83c8b06f4da5bacc36b709514bd550a92a149e9fa1d
 
-$(package)_libsodium_version=1.0.18
+$(package)_libsodium_version=1.0.18-stable-msvc
 $(package)_libsodium_download_path=https://download.libsodium.org/libsodium/releases
-$(package)_libsodium_download_file=libsodium-$($(package)_libsodium_version).tar.gz
-$(package)_libsodium_file_name=libsodium-$($(package)_libsodium_version).tar.gz
+$(package)_libsodium_download_file=libsodium-$($(package)_libsodium_version).zip
+$(package)_libsodium_file_name=libsodium-$($(package)_libsodium_version).zip
 sspackage)_libsodium_build_subdir=sodium-populate
 $(package)_libsodium_sha256_hash=6f504490b342a4f8a4c4a02fc9b866cbef8622d5df4e5452b46be121e46636c1
 $(package)_extra_sources=$($(package)_relic_file_name)
