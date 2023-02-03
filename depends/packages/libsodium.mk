beba78a4aabf63c75717e719799dd895928a20d6
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-02-03 12:59:04 +0000

    Change libsodium build directory

--- depends/packages/libsodium.mk
+++ depends/packages/libsodium.mk
@@ -2,7 +2,6 @@ package=libsodium
 $(package)_version=1.0.18
 $(package)_download_path=https://download.libsodium.org/libsodium/releases
 $(package)_file_name=libsodium-$($(package)_version).tar.gz
-$(package)_build_subdir=build
 $(package)_sha256_hash=6f504490b342a4f8a4c4a02fc9b866cbef8622d5df4e5452b46be121e46636c1
 
 define $(package)_set_vars
@@ -28,7 +27,7 @@ define $(package)_config_cmds
   export CFLAGS="$($(package)_cflags) $($(package)_cppflags)" && \
   export CXXFLAGS="$($(package)_cxxflags) $($(package)_cppflags)" && \
   export LDFLAGS="$($(package)_ldflags)" && \
-  .,/configure --with-pic="yes"
+  ./configure --with-pic="yes"
 endef
 
 define $(package)_build_cmds
