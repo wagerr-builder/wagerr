21ccb7e74e9da45c10fcab09085619486af97507
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-02-03 11:37:26 +0000

    Change guix commit

--- contrib/guix/libexec/prelude.bash
+++ contrib/guix/libexec/prelude.bash
@@ -51,7 +51,7 @@ fi
 time-machine() {
     # shellcheck disable=SC2086
     guix time-machine --url=https://git.savannah.gnu.org/git/guix.git \
-                      --commit=998eda3067c7d21e0d9bb3310d2f5a14b8f1c681 \
+                      --commit=270e1b9e1ea2b3e41067a38b094b0656ceb56838 \
                       --cores="$JOBS" \
                       --keep-failed \
                       --fallback \
