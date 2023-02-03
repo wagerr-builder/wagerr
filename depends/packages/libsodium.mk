17e3cc3ebd9d36cba4c79f492ad01652cb4afa0e
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-02-03 12:20:32 +0000

    Add libsodium as extra package

--- depends/packages/libsodium.mk
+++ depends/packages/libsodium.mk
@@ -1,34 +1,20 @@
-package=bls-dash
-$(package)_version=1.2.0
-$(package)_download_path=https://github.com/dashpay/bls-signatures/archive
-$(package)_download_file=$($(package)_version).tar.gz
+package=libsodium
+$(package)_version=1.0.18
+$(package)_download_path=https://download.libsodium.org/libsodium/releases/libsodium-1.0.18-stable.tar.gz
+$(package)_download_file=libsodium-$($(package)_version).tar.gz
 $(package)_file_name=$(package)-$($(package)_download_file)
 $(package)_build_subdir=build
-$(package)_sha256_hash=94e49f3eaa29bc1f354cd569c00f4f4314d1c8ab4758527c248b67da9686135a
-$(package)_dependencies=gmp cmake libsodium
-$(package)_darwin_triplet=x86_64-apple-darwin19
-
-$(package)_relic_version=aecdcae7956f542fbee2392c1f0feb0a8ac41dc5
-$(package)_relic_download_path=https://github.com/relic-toolkit/relic/archive
-$(package)_relic_download_file=$($(package)_relic_version).tar.gz
-$(package)_relic_file_name=relic-$($(package)_relic_download_file)
-$(package)_relic_build_subdir=relic
-$(package)_relic_sha256_hash=f2de6ebdc9def7077f56c83c8b06f4da5bacc36b709514bd550a92a149e9fa1d
-
-$(package)_extra_sources=$($(package)_relic_file_name)
+$(package)_sha256_hash=edba9dd57b03cfd95d677709204bb13666ac1336c76c79bca8a51b15a1ccf3ac
 
 define $(package)_fetch_cmds
 $(call fetch_file,$(package),$($(package)_download_path),$($(package)_download_file),$($(package)_file_name),$($(package)_sha256_hash)) && \
-$(call fetch_file,$(package),$($(package)_relic_download_path),$($(package)_relic_download_file),$($(package)_relic_file_name),$($(package)_relic_sha256_hash))
 endef
 
 define $(package)_extract_cmds
   mkdir -p $($(package)_extract_dir) && \
   echo "$($(package)_sha256_hash)  $($(package)_source)" > $($(package)_extract_dir)/.$($(package)_file_name).hash && \
-  echo "$($(package)_relic_sha256_hash)  $($(package)_source_dir)/$($(package)_relic_file_name)" >> $($(package)_extract_dir)/.$($(package)_file_name).hash && \
   $(build_SHA256SUM) -c $($(package)_extract_dir)/.$($(package)_file_name).hash && \
   tar --strip-components=1 -xf $($(package)_source) -C . && \
-  cp $($(package)_source_dir)/$($(package)_relic_file_name) .
 endef
 
 define $(package)_set_vars
@@ -45,18 +31,7 @@ define $(package)_set_vars
   $(package)_config_opts_armv7l+= -DWSIZE=32
   $(package)_config_opts_debug=-DDEBUG=ON -DCMAKE_BUILD_TYPE=Debug
 
-  ifneq ($(darwin_native_toolchain),)
-    $(package)_config_opts_darwin+= -DCMAKE_AR="$(host_prefix)/native/bin/$($(package)_darwin_triplet)-ar"
-    $(package)_config_opts_darwin+= -DCMAKE_LINKER="$(host_prefix)/native/bin/$($(package)_darwin_triplet)-ld"
-    $(package)_config_opts_darwin+= -DCMAKE_RANLIB="$(host_prefix)/native/bin/$($(package)_darwin_triplet)-ranlib"
-  endif
-
   $(package)_cppflags+=-UBLSALLOC_SODIUM
-endef
-
-define $(package)_preprocess_cmds
-  sed -i.old "s|GIT_REPOSITORY https://github.com/Chia-Network/relic.git|URL \"../../relic-$($(package)_relic_version).tar.gz\"|" CMakeLists.txt && \
-  sed -i.old "s|RELIC_GIT_TAG \".*\"|RELIC_GIT_TAG \"\"|" CMakeLists.txt
 endef
 
 define $(package)_config_cmds
@@ -65,7 +40,7 @@ define $(package)_config_cmds
   export CFLAGS="$($(package)_cflags) $($(package)_cppflags)" && \
   export CXXFLAGS="$($(package)_cxxflags) $($(package)_cppflags)" && \
   export LDFLAGS="$($(package)_ldflags)" && \
-  $(host_prefix)/bin/cmake ../ $($(package)_config_opts)
+  ./configure --with-pic="yes"
 endef
 
 define $(package)_build_cmds
