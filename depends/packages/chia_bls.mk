cfb9d8697d2f4be855a196d6d224edcc2220b9be
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-02-03 14:11:20 +0000

    FInd correct path for cmake

--- depends/packages/chia_bls.mk
+++ depends/packages/chia_bls.mk
@@ -45,7 +45,7 @@ define $(package)_config_cmds
   export CXXFLAGS="$($(package)_cxxflags) $($(package)_cppflags)" && \
   export LDFLAGS="$($(package)_ldflags)" && \
   mkdir -p build && cd build && \
-  depends/x86_64-pc-linux-gnu/bin/cmake ../ $($(package)_config_opts)
+  $($(package)_staging_dir)/cmake ../ $($(package)_config_opts)
 endef
 
 define $(package)_build_cmds
