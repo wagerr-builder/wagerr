5d37618810d26ceebed8e01b57c88ddf2924df0c
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-02-10 10:58:17 +0000

    7

--- src/qt/clientmodel.cpp
+++ src/qt/clientmodel.cpp
@@ -349,3 +349,13 @@ bool ClientModel::getProxyInfo(std::string& ip_port) const
     }
     return false;
 }
+
+bool ClientModel::getOnionInfo(std::string& ip_port) const
+{
+    proxyType ipv4, ipv6;
+    if (m_node.getProxy((Network) 1, ipv4) && m_node.getProxy((Network) 2, ipv6)) {
+      ip_port = ipv4.proxy.ToStringIPPort();
+      return true;
+    }
+    return false;
+}
