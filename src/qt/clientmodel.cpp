3915c73b2a694de6e8c787f09670ce475cec6287
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-02-10 11:59:15 +0000

    21

--- src/qt/clientmodel.cpp
+++ src/qt/clientmodel.cpp
@@ -352,7 +352,7 @@ bool ClientModel::getProxyInfo(std::string& ip_port) const
 
 bool ClientModel::getOnionInfo(std::string& ip_port) const
 {
-    bool proxyRandomize = false);
+    bool proxyRandomize = false;
     CService onionProxy;
     proxyType addrOnion = proxyType(onionProxy, proxyRandomize);
     if (addrOnion.IsValid()) {
