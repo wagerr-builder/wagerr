cf0be49378f8eb45f9d48d1c98ef46b0f53b6fc4
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-11 10:43:44 +0000

    CHeck for errors in parlaybet 2

--- test/functional/feature_hybrid_betting.py
+++ test/functional/feature_hybrid_betting.py
@@ -406,8 +406,8 @@ class HybridBettingTest(WagerrTestFramework):
         ]
 
         # try to place hybrid bet with one leg
-        assert_raises_rpc_error(-31, "Error: Incorrect legs count.",
-            self.nodes[2].placehybridparlaybet, player1_legs[:1], 100)
+        #assert_raises_rpc_error(-31, "Error: Incorrect legs count.",
+        self.nodes[2].placehybridparlaybet(player1_legs[:1], 100)
 
         # invalid amount
         #assert_raises_rpc_error(-31, "Incorrect bet amount. Please ensure your bet is between 25 - 10000 WGR inclusive.",
