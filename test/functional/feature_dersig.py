55d1a2c8039edc54bb31168c1114f9b0206eb72d
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-20 15:57:17 +0000

    Add mn_count to feature dersig

--- test/functional/feature_dersig.py
+++ test/functional/feature_dersig.py
@@ -49,6 +49,8 @@ class BIP66Test(WagerrTestFramework):
         self.extra_args = [['-whitelist=127.0.0.1', '-dip3params=9000:9000', '-par=1', '-enablebip61']]  # Use only one script thread to get the exact reject reason for testing
         self.setup_clean_chain = True
         self.rpc_timeout = 120
+        self.mn_count = 0
+        self.fast_dip3_enforcement = False
 
     def test_dersig_info(self, *, is_active):
         assert_equal(
