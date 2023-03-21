bd1c5839d2c0962fc3aaf8f7e118f69a848bb0ca
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-21 15:21:46 +0000

    Remove address from generate 13

--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -689,7 +689,7 @@ class WagerrTestFramework(metaclass=WagerrTestMetaClass):
                 chain = get_chain_folder(cache_node_dir, self.chain)
                 return os.path.join(cache_node_dir, chain, *paths)
 
-            os.rmdir(cache_path('wallets'))  # Remove empty wallets dir
+            #os.rmdir(cache_path('wallets'))  # Remove empty wallets dir
             for entry in os.listdir(cache_path()):
                 if entry not in ['wallets', 'chainstate', 'blocks', 'evodb', 'llmq', 'backups', 'tokens', 'zerocoin']:
                     os.remove(cache_path(entry))
