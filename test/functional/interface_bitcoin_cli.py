a54d97e50f5762b4fa6992b60d62f4d3568b7970
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-23 14:26:00 +0000

    Add exta args and mn count to bitcoin cli

--- test/functional/interface_bitcoin_cli.py
+++ test/functional/interface_bitcoin_cli.py
@@ -11,6 +11,9 @@ class TestBitcoinCli(WagerrTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
         self.num_nodes = 1
+        self.mn_count = 0
+        self.fast_dip3_enforcement = False
+        self.extra_args = [["-debug"]]
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_cli()
