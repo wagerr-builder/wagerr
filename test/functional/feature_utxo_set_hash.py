094c8a9b9a79f6dcad969026e562c6b1d9d31c6e
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-23 12:39:29 +0000

    Change hash in utxo set hash 2

--- test/functional/feature_utxo_set_hash.py
+++ test/functional/feature_utxo_set_hash.py
@@ -31,7 +31,7 @@ class UTXOSetHashTest(WagerrTestFramework):
         self.log.info("Test deterministic UTXO set hash results")
 
         # These depend on the setup_clean_chain option, the chain loaded from the cache
-        assert_equal(self.nodes[0].gettxoutsetinfo()['hash_serialized_2'], "5d53aeacb4acd25f88e77144c98ec024655c8b4e4b16fdcfdb26058a0ef7c7a0")
+        assert_equal(self.nodes[0].gettxoutsetinfo()['hash_serialized_2'], "bf47168f99200d859c959df075782046ce4228d9fe43aee8ee078ec9ad5ca0f3")
         assert_equal(self.nodes[0].gettxoutsetinfo("muhash")['muhash'], "dd5ad2a105c2d29495f577245c357409002329b9f4d6182c0af3dc2f462555c8")
 
     def test_muhash_implementation(self):
