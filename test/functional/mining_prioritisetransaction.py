d93dcef2b55dbb7b101380fec8a273648cb40709
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-24 11:18:09 +0000

    Add mn count to mining prioritize transaction

--- test/functional/mining_prioritisetransaction.py
+++ test/functional/mining_prioritisetransaction.py
@@ -17,6 +17,8 @@ class PrioritiseTransactionTest(WagerrTestFramework):
             "-acceptnonstdtxn=1",
         ]] * self.num_nodes
         self.supports_cli = False
+        self.mn_count = 0
+        self.fast_dip3_enforcement = False
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
