ac5f94f8cedd039bdc58278cedab2c306f013157
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-25 11:58:15 +0000

    Add mn count and extra args to invalid messages

--- test/functional/p2p_invalid_messages.py
+++ test/functional/p2p_invalid_messages.py
@@ -31,6 +31,9 @@ class InvalidMessagesTest(WagerrTestFramework):
     def set_test_params(self):
         self.num_nodes = 1
         self.setup_clean_chain = True
+        self.mn_count = 0
+        self.fast_dip3_enforcement = False
+        self.extra_args = [["-debug"]]
 
     def run_test(self):
         """
