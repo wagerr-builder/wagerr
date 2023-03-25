c719b563b74636b479fcd14f3bc01ba1e4bc9557
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-25 10:44:08 +0000

    Add spork key to instantsend

--- test/functional/p2p_instantsend.py
+++ test/functional/p2p_instantsend.py
@@ -15,6 +15,7 @@ Tests InstantSend functionality (prevent doublespend for unconfirmed transaction
 class InstantSendTest(WagerrTestFramework):
     def set_test_params(self):
         self.set_wagerr_test_params(7, 3, fast_dip3_enforcement=True)
+        self.extra_args = [["-sporkkey=6xLZdACFRA53uyxz8gKDLcgVrm5kUUEu2B3BUzWUxHqa2W7irbH"], ["-sporkkey=6xLZdACFRA53uyxz8gKDLcgVrm5kUUEu2B3BUzWUxHqa2W7irbH"], ["-sporkkey=6xLZdACFRA53uyxz8gKDLcgVrm5kUUEu2B3BUzWUxHqa2W7irbH"], ["-sporkkey=6xLZdACFRA53uyxz8gKDLcgVrm5kUUEu2B3BUzWUxHqa2W7irbH"]]
         # set sender,  receiver,  isolated nodes
         self.isolated_idx = 1
         self.receiver_idx = 2
