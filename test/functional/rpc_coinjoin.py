f82d43e1e08dd3922b4ade1efa5769e243201fa2
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-26 12:22:57 +0100

    Add mn count and extra args to rpc coinjoin

--- test/functional/rpc_coinjoin.py
+++ test/functional/rpc_coinjoin.py
@@ -15,6 +15,9 @@ Tests CoinJoin basic RPC
 class CoinJoinTest(WagerrTestFramework):
     def set_test_params(self):
         self.num_nodes = 1
+        self.mn_count = 0
+        self.fast_dip3_enforcement = False
+        self.extra_args = [["-debug"]]
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
