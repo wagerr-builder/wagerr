9b6c514ddcce13ce431eb2830b811dea338a4958
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-26 11:43:34 +0100

    Change to stio start rather than restart in rpc blockchain

--- test/functional/rpc_blockchain.py
+++ test/functional/rpc_blockchain.py
@@ -59,7 +59,8 @@ class BlockchainTest(WagerrTestFramework):
 
     def run_test(self):
         self.mine_chain()
-        self.restart_node(0, extra_args=['-stopatheight=238', '-prune=1', '-txindex=0'])  # Set extra args with pruning after rescan is complete
+        self.stop_node(0)
+        self.start_node(0, extra_args=['-stopatheight=238', '-prune=1', '-txindex=0'])  # Set extra args with pruning after rescan is complete
 
         # Actual tests
         self._test_getblockchaininfo()
