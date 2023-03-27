85aed8845393b3d0288f01f44f295bf32c501875
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-27 14:23:49 +0100

    Add n count and extra args to rpc misc

--- test/functional/rpc_misc.py
+++ test/functional/rpc_misc.py
@@ -20,6 +20,9 @@ class RpcMiscTest(WagerrTestFramework):
     def set_test_params(self):
         self.num_nodes = 1
         self.supports_cli = False
+        self.mn_count = 0
+        self.fast_dip3_enforcement = False
+        self.extra_args = [["-debug"]]
 
     def run_test(self):
         node = self.nodes[0]
