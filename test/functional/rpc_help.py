054594d85d1b595c223c3fc46457920d4528f98e
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-27 13:05:53 +0100

    Add Tokens to rpc help

--- test/functional/rpc_help.py
+++ test/functional/rpc_help.py
@@ -39,7 +39,7 @@ class HelpRpcTest(WagerrTestFramework):
         # command titles
         titles = [line[3:-3] for line in node.help().splitlines() if line.startswith('==')]
 
-        components = ['Addressindex','Betting', 'Blockchain', 'Control', 'Wagerr', 'Evo', 'Generating', 'Mining', 'Network', 'Rawtransactions', 'Util']
+        components = ['Addressindex','Betting', 'Blockchain', 'Control', 'Wagerr', 'Evo', 'Generating', 'Mining', 'Network', 'Rawtransactions', 'Tokens', 'Util']
 
         if self.is_wallet_compiled():
             components.append('Wallet')
