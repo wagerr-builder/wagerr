867a4cccfeaa269598dd86d7abea5056ef3cb443
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-28 10:33:54 +0100

    Add mn count and extra args to rpc scantxoutset

--- test/functional/rpc_scantxoutset.py
+++ test/functional/rpc_scantxoutset.py
@@ -17,6 +17,9 @@ class ScantxoutsetTest(WagerrTestFramework):
         self.num_nodes = 1
         self.setup_clean_chain = True
         self.supports_cli = False
+        self.mn_count = 0
+        self.fast_dip3_enforcement = False
+        self.extra_args = [["-debug"]]
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
