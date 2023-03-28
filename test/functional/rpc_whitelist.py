f64f89785677dacaaf8dc1b25d016f33d655bb47
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-28 11:00:33 +0100

    Add mn count and extra args to rpc whitelist

--- test/functional/rpc_whitelist.py
+++ test/functional/rpc_whitelist.py
@@ -30,6 +30,9 @@ class RPCWhitelistTest(WagerrTestFramework):
     def set_test_params(self):
         self.num_nodes = 1
         self.supports_cli = False
+        self.mn_count = 0
+        self.fast_dip3_enforcement = False
+        self.extra_args = [["-debug"]]
 
     def setup_chain(self):
         super().setup_chain()
