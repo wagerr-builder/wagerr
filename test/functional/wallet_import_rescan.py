75a0e7180e5f759968654528aa3abd92df97e978
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-29 12:39:34 +0100

    Change startup method in wallet import rescan 4

--- test/functional/wallet_import_rescan.py
+++ test/functional/wallet_import_rescan.py
@@ -140,7 +140,7 @@ class ImportRescanTest(WagerrTestFramework):
             n.importprivkey(privkey=n.get_deterministic_priv_key().key, label='coinbase')
 
         for i in range(0, self.num_nodes -1):
-            self.restart_nodes(i)
+            self.restart_node(i)
 
         for i in range(1, self.num_nodes):
             connect_nodes(self.nodes[i], 0)
