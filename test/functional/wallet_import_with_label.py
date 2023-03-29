ad75551a4f0e6a415116d02591c0501467610a14
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-29 13:13:46 +0100

    Add mn_count to wallet import with label

--- test/functional/wallet_import_with_label.py
+++ test/functional/wallet_import_with_label.py
@@ -18,11 +18,9 @@ class ImportWithLabel(WagerrTestFramework):
     def set_test_params(self):
         self.num_nodes = 2
         self.setup_clean_chain = True
+        self.mn_count = 0
+        self.fast_dip3_enforcement = False
         self.extra_args = [["-debug"], ["-debug"]]
-
-    def setup_network(self):
-        self.add_nodes(self.num_nodes, self.extra_args)
-        self.start_nodes()
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
