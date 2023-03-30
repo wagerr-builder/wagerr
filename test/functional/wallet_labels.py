aa3760059a6723e8cf39f18a10f3d3e7b0f1fb39
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-30 09:57:00 +0100

    Remove generatetoadress in wallet labels 2

--- test/functional/wallet_labels.py
+++ test/functional/wallet_labels.py
@@ -35,7 +35,7 @@ class WalletLabelsTest(WagerrTestFramework):
         # Note each time we call generate, all generated coins go into
         # the same address, so we call twice to get two addresses w/500 each
         node.generate(1)
-        node.generate(101, address=node.getnewaddress(label='coinbase'))
+        node.generate(101)
         assert_equal(node.getbalance(), 195860471)
 
         # there should be 2 address groups
