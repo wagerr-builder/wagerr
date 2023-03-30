5438da36da0d4e9d19298d1559ab91a128a075f6
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-30 14:39:48 +0100

    Change to setup_network in wallet txn doublespend

--- test/functional/wallet_txn_doublespend.py
+++ test/functional/wallet_txn_doublespend.py
@@ -17,6 +17,11 @@ class TxnMallTest(WagerrTestFramework):
     def set_test_params(self):
         self.num_nodes = 4
         self.supports_cli = False
+        self.extra_args = [["-debug"], ["-debug"], ["-debug"], ["-debug"]]
+
+    def setup_network(self):
+        self.add_nodes(self.num_nodes, self.extra_args)
+        self.start_nodes()
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
