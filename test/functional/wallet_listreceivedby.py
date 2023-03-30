9cbac062503f808e1d94201cc6615ba213283b3f
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-30 13:13:58 +0100

    Change to setup_network in wallet listreceivedby

--- test/functional/wallet_listreceivedby.py
+++ test/functional/wallet_listreceivedby.py
@@ -19,6 +19,11 @@ from test_framework.util import (
 class ReceivedByTest(WagerrTestFramework):
     def set_test_params(self):
         self.num_nodes = 2
+        self.extra_args = [["-debug"], ["-debug"]]
+
+    def setup_network(self):
+        self.add_nodes(self.num_nodes, self.extra_args)
+        self.start_nodes()
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
