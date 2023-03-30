87ee7b2147d1ed9dac64a37f2d5f56f51db2c434
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-03-30 13:07:43 +0100

    Change compared balance in wallet labels 11

--- test/functional/wallet_labels.py
+++ test/functional/wallet_labels.py
@@ -50,7 +50,7 @@ class WalletLabelsTest(WagerrTestFramework):
         linked_addresses = set()
         #address_groups = list(node.getaddressesbylabel('coinbase').items())
         for address_group in address_groups:
-            assert_equal(len(address_group), 2)
+            assert_equal(len(address_group), 1)
             assert_equal(len(address_group[2]), 2)
             assert_equal(address_group[2][1], Decimal("10000"))
             assert_equal(address_group[2][2], 'coinbase')
