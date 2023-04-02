dc72ba2a2117b7ebc2061ec1dda8a26a8dfce3c3
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-04-02 12:58:50 +0100

    Use setup_network in mempool resurrect

--- test/functional/mempool_resurrect.py
+++ test/functional/mempool_resurrect.py
@@ -12,9 +12,11 @@ from test_framework.util import assert_equal
 class MempoolCoinbaseTest(WagerrTestFramework):
     def set_test_params(self):
         self.num_nodes = 1
-        self.mn_count = 0
-        self.fast_dip3_enforcement = False
         self.extra_args = [["-debug"]]
+
+    def setup_network(self):
+        self.add_nodes(self.num_nodes, self.extra_args)
+        self.start_nodes()
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
