64f6d5e6d10eb8ff02fe9cf7f7bc5b25b5df1496
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-04-02 12:15:08 +0100

    Use setup_network in interface zmq

--- test/functional/interface_zmq.py
+++ test/functional/interface_zmq.py
@@ -43,6 +43,11 @@ class ZMQSubscriber:
 class ZMQTest (WagerrTestFramework):
     def set_test_params(self):
         self.num_nodes = 2
+        self.extra_args = [[], []]
+
+    def setup_network(self):
+        self.add_nodes(self.num_nodes, self.extra_args)
+        self.start_nodes()
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_py3_zmq()
