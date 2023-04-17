23e79012e0f0866def8d2a503153b86c73f0e87f
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-04-17 14:42:01 +0100

    Revert "Add CBettingsView to miner_tests 2"
    This reverts commit bb82e748b497ebe46ee25513d661ec755b06e5ae.

--- src/test/miner_tests.cpp
+++ src/test/miner_tests.cpp
@@ -40,16 +40,15 @@ struct MinerTestingSetup : public TestingSetup {
     BlockAssembler AssemblerForTest(const CChainParams& params);
     CBettingsView phr;
 
-    // Constructor for MinerTestingSetup
-    MinerTestingSetup();
+    // Add a constructor for MinerTestingSetup
+    MinerTestingSetup()
+    {
+        // Initialize the CBettingsView object
+        phr = CBettingsView();
+    }
 };
 } // namespace miner_tests
 
-miner_tests::MinerTestingSetup::MinerTestingSetup()
-    : TestingSetup(CBaseChainParams::REGTEST)
-    , phr() // Initialize phr using the default constructor
-{
-}
 
 BOOST_FIXTURE_TEST_SUITE(miner_tests, MinerTestingSetup)
 
@@ -217,9 +216,9 @@ void MinerTestingSetup::TestPackageSelection(const CChainParams& chainparams, co
 }
 
 // NOTE: These tests rely on CreateNewBlock doing its own self-validation!
-BOOST_AUTO_TEST_CASE(CreateNewBlock_validity, MinerTestingSetup)
+BOOST_AUTO_TEST_CASE(CreateNewBlock_validity)
 {
-    const auto chainParams = CreateChainParams(CBaseChainParams::REGTEST);
+    const auto chainParams = CreateChainParams(CBaseChainParams::MAIN);
     const CChainParams& chainparams = *chainParams;
     CScript scriptPubKey = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
     std::unique_ptr<CBlockTemplate> pblocktemplate, pemptyblocktemplate;
