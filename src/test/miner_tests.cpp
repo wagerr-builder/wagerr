92a12b110273309702771f78fbf61596c0e09edd
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-04-18 11:07:36 +0100

    Add CBettingsView to miner_tests 10

--- src/test/miner_tests.cpp
+++ src/test/miner_tests.cpp
@@ -32,6 +32,8 @@
 
 #include <betting/bet_db.h>
 
+#include <betting/bet_db.h>
+
 namespace miner_tests {
     struct MinerTestingSetup : public TestingSetup {
         void TestPackageSelection(const CChainParams& chainparams, const CScript& scriptPubKey, const std::vector<CTransactionRef>& txFirst) EXCLUSIVE_LOCKS_REQUIRED(::cs_main, m_node.mempool->cs);
@@ -218,8 +220,10 @@ void MinerTestingSetup::TestPackageSelection(const CChainParams& chainparams, co
 }
 
 // NOTE: These tests rely on CreateNewBlock doing its own self-validation!
-BOOST_FIXTURE_TEST_CASE(CreateNewBlock_validity, MinerTestingSetup)
-{
+BOOST_FIXTURE_TEST_CASE(CreateNewBlock_validity, miner_tests::MinerTestingSetup) {
+    // You can now access the phr object directly inside the test case
+    // For example:
+    CBettingsView* testPhr = &phr;
     const auto chainParams = CreateChainParams(CBaseChainParams::MAIN);
     const CChainParams& chainparams = *chainParams;
     CScript scriptPubKey = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
