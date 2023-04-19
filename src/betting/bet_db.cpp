7eff0ca111cd0b5335b4b83c1e3358ea48b68301
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-04-19 12:50:37 +0100

    Add nullptr case for CBettingsView in bet db cpp 7

--- src/betting/bet_db.cpp
+++ src/betting/bet_db.cpp
@@ -698,10 +698,10 @@ CBettingsView::CBettingsView(CBettingsView* phr) {
         chainGamesLottoResults = MakeUnique<CBettingDB>(*phr->chainGamesLottoResults.get());
         failedBettingTxs = MakeUnique<CBettingDB>(*phr->failedBettingTxs.get());
     } else {
-        Initialize();
+        Initialize(phr);
     }
 }
-void CBettingsView::Initialize() {
+void CBettingsView::Initialize(phr) {
     if (this == nullptr) {
         phr = new CBettingsView();
         // Create storage instances
