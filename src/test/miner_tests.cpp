c027814e9f8867b3571fabc6641ce32bcc09e14a
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-04-22 14:38:55 +0100

    Change block creation in miner tests

--- src/test/miner_tests.cpp
+++ src/test/miner_tests.cpp
@@ -221,8 +221,8 @@ BOOST_AUTO_TEST_CASE(CreateNewBlock_validity)
     fCheckpointsEnabled = false;
 
     // Simple block creation, nothing special yet:
-    BOOST_CHECK(pemptyblocktemplate = AssemblerForTest(chainparams).CreateNewBlock(scriptPubKey));
-
+    BlockTemplate* pemptyblocktemplate;
+    pemptyblocktemplate = AssemblerForTest(chainparams).CreateNewBlock(scriptPubKey);
     // We can't make transactions until we have inputs
     // Therefore, load 100 blocks :)
     int baseheight = 0;
