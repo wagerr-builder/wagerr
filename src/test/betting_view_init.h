70e4c35580f1f35e067e82b7cbfba4add1716668
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-04-23 10:14:46 +0100

    Add bet_db initialization in tests

---
+++ src/test/betting_view_init.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 The Wagerr developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BETTING_VIEW_INIT_H
+#define BETTING_VIEW_INIT_H
+
+#include <memory>
+#include <betting/bet_db.h> // Include the header file for CBettingsView class
+
+CBettingsView* initializeBettingView(std::unique_ptr<CBettingsView> &bettingsView, bool fReindex);
+
+#endif // BETTING_VIEW_INIT_H
