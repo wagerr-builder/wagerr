7592ccf288d5a885b9189a1ba96e597901c9fbfb
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-04-25 12:31:56 +0100

    Add a test environment variable to testing 2

--- src/betting/bet_db.cpp
+++ src/betting/bet_db.cpp
@@ -5,6 +5,7 @@
 #include <betting/bet_db.h>
 #include <boost/format.hpp>
 
+#include <validation.h>
 #include <test/test_env.h>
 
 #define DOUBLE_ODDS(odds) (static_cast<double>(odds) / BET_ODDSDIVISOR)
