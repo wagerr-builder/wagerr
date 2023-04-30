685f66c117e08fdcec32e0157f54c747fa64f781
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-04-30 11:13:36 +0100

    Add in copyright notices

--- src/test/test_utils.cpp
+++ src/test/test_utils.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2023 The Wagerr Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include "test_utils.h"
 
 bool IsTestEnvironment() {
--- src/test/test_wallet_init.cpp
+++ src/test/test_wallet_init.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2023 The Wagerr Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include "wallet/wallet.h"
 #include "wallet/walletutil.h"
 #include "interfaces/chain.h"
