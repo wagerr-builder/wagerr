d6a3949d51369dac2f0755dcb0e931cd3f986a70
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-05-15 11:19:58 +0100

    Change registration in deterministicmns

--- test/functional/feature_dip3_deterministicmns.py
+++ test/functional/feature_dip3_deterministicmns.py
@@ -249,7 +249,7 @@ class DIP3Test(WagerrTestFramework):
 
     # register a protx MN and also fund it (using collateral inside ProRegTx)
     def register_fund_mn(self, node, mn):
-        mn.collateral_txid = node.sendtoaddress(mn.fundsAddr, 25000.001)
+        mn.collateral_txid = node.sendtoaddress(mn.fundsAddr, 25000)
         mn.collateral_vout = 0
         txraw = self.nodes[0].getrawtransaction(mn.collateral_txid, True)
         for vout_idx in range(0, len(txraw["vout"])):
@@ -273,7 +273,10 @@ class DIP3Test(WagerrTestFramework):
     # create a protx MN which refers to an existing collateral
     def register_mn(self, node, mn):
         breakpoint()
-        node.sendtoaddress(mn.fundsAddr, 25000.001)
+        node.sendtoaddress(mn.fundsAddr, 25000)
+        node.generate(1)
+        node.sendtoaddress(mn.fundsAddr, 0.001)
+        node.generate(1)
         mn.rewards_address = node.getnewaddress()
         mn.protx_hash = node.protx('register', mn.collateral_txid, mn.collateral_vout, '127.0.0.1:%d' % mn.p2p_port, mn.ownerAddr, mn.operatorAddr, mn.votingAddr, 0, mn.rewards_address, mn.fundsAddr)
         node.generate(1)
