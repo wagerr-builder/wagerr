21bc30bf8377dcbb8cebb7fa6137b034a1c72b6d
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-05-17 13:33:02 +0100

    Remove manual mining in  deterministicmns

--- test/functional/feature_dip3_deterministicmns.py
+++ test/functional/feature_dip3_deterministicmns.py
@@ -381,6 +381,8 @@ class DIP3Test(WagerrTestFramework):
 
     def mine_block(self, node, vtx=[], miner_address=None, mn_payee=None, mn_amount=None, use_mnmerkleroot_from_tip=False, expected_error=None):
         breakpoint()
+        node.generate(1)
+        """ getblocktemplate does not work in POS
         bt = node.getblocktemplate()
         height = bt['height']
         tip_hash = bt['previousblockhash']
@@ -463,7 +465,7 @@ class DIP3Test(WagerrTestFramework):
             raise AssertionError('mining the block should have failed with error %s, but submitblock returned %s' % (expected_error, result))
         elif expected_error is None and result is not None:
             raise AssertionError('submitblock returned %s' % (result))
-
+        """
     def mine_double_spend(self, node, txins, target_address, use_mnmerkleroot_from_tip=False):
         amount = Decimal(0)
         for txin in txins:
