6acb859760b34372abf5ab707e9037790f7213d5
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-05-20 14:04:14 +0100

    Change mn list in test protx update service in deterministicmns

--- test/functional/feature_dip3_deterministicmns.py
+++ test/functional/feature_dip3_deterministicmns.py
@@ -333,7 +333,7 @@ class DIP3Test(WagerrTestFramework):
             mn_list = node.masternodelist('status')
             assert_equal(protx_info['state']['service'], '127.0.0.2:%d' % mn.p2p_port)
             breakpoint()
-            assert_equal(mn_list['%s-%d' % (mn.collateral_txid, mn.collateral_vout)]['address'], '127.0.0.2:%d' % mn.p2p_port)
+            assert_equal(mn_list['%s-%d' % (mn.collateral_txid, mn.collateral_vout)], 'ENABLED')
 
         # undo
         self.nodes[0].protx('update_service', mn.protx_hash, '127.0.0.1:%d' % mn.p2p_port, mn.blsMnkey, "", mn.fundsAddr)
