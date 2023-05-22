585beb50c6998db7d5d53c91ed8bbb33d562da4d
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-05-22 12:17:16 +0100

    CHange reversion in deterministicmns 6

--- test/functional/feature_dip3_deterministicmns.py
+++ test/functional/feature_dip3_deterministicmns.py
@@ -138,7 +138,7 @@ class DIP3Test(WagerrTestFramework):
 
         self.log.info("test that reverting the blockchain on a single node results in the mnlist to be reverted as well")
         for i in range(spend_mns_count):
-            for j in range(self.num_nodes - 2):
+            for j in range(self.num_nodes - 1):
                 self.nodes[j].invalidateblock(self.nodes[j].getbestblockhash())
             mns_tmp.append(mns[spend_mns_count - 1 - i])
             self.assert_mnlist(self.nodes[0], mns_tmp)
