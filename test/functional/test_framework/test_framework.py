a4721530815d42fb704b0f6e782389b8d1843054
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-05-22 15:00:01 +0100

    Rearrange funding in test framework masternode preparation

--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -909,7 +909,7 @@ class WagerrTestFramework(WagerrTestFramework):
             self.nodes[0].generate(1)
             protx_result = self.nodes[0].protx('register', txid, collateral_vout, ipAndPort, ownerAddr, bls['public'], votingAddr, operatorReward, rewardsAddr, address, submit)
         """
-        self.nodes[0].generate(1)
+        breakpoint()
         protx_result = self.nodes[0].protx('register', txid, collateral_vout, ipAndPort, ownerAddr, bls['public'], votingAddr, operatorReward, rewardsAddr, address, submit)
 
         if submit:
