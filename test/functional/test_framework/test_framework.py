30f94dcd712350c75964daa70b7cbf85b4526d9e
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-05-24 12:54:39 +0100

    Revert quorum to default in test framework

--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -1250,11 +1250,10 @@ class WagerrTestFramework(WagerrTestFramework):
 
     def wait_for_quorum_phase(self, quorum_hash, phase, expected_member_count, check_received_messages, check_received_messages_count, mninfos, llmq_type_name="llmq_test", timeout=30, sleep=1):
         def check_dkg_session():
-            breakpoint()
             all_ok = True
             member_count = 0
             for mn in mninfos:
-                s = mn.node.quorum("dkgstatus")
+                s = mn.node.quorum("dkgstatus")["session"]
                 mn_ok = True
                 for qs in s:
                     if qs["llmqType"] != llmq_type_name:
