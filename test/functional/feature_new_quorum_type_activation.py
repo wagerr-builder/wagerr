ac32c50e40068d52d18c49ea01575bb85044c137
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-06-04 12:19:25 +0100

    Add spork keys and settings to feature new quorum type

--- test/functional/feature_new_quorum_type_activation.py
+++ test/functional/feature_new_quorum_type_activation.py
@@ -16,9 +16,17 @@ Tests the activation of a new quorum type in dip0020 via a bip9-like hardfork
 class NewQuorumTypeActivationTest(WagerrTestFramework):
     def set_test_params(self):
         self.extra_args = [["-vbparams=dip0020:0:999999999999:10:8:6:5"]]
+        self.extra_args += [["-sporkkey=6xLZdACFRA53uyxz8gKDLcgVrm5kUUEu2B3BUzWUxHqa2W7irbH"]] * 4
         self.set_wagerr_test_params(4, 3, fast_dip3_enforcement=False)
 
     def run_test(self):
+        for i in range(len(self.nodes)):
+                self.nodes[i].sporkupdate("SPORK_4_DIP0003_ENFORCED", 1)
+                self.nodes[i].sporkupdate("SPORK_17_QUORUM_DKG_ENABLED", 1)
+                self.nodes[i].sporkupdate("SPORK_21_QUORUM_ALL_CONNECTED", 1)
+                self.nodes[i].sporkupdate("SPORK_23_QUORUM_POSE", 1)
+
+        self.wait_for_sporks_same()
         #assert_equal(get_bip9_status(self.nodes[0], 'dip0020')['status'], 'defined')
         self.nodes[0].generate(9)
         #assert_equal(get_bip9_status(self.nodes[0], 'dip0020')['status'], 'started')
