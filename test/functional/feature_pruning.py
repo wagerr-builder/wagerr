b8742b6b0123d7ff8f2d0c42532c2cfdbd866931
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-06-05 13:51:32 +0100

    Change tocreate transactions in feature pruning 2

--- test/functional/feature_pruning.py
+++ test/functional/feature_pruning.py
@@ -22,7 +22,7 @@ from test_framework.util import assert_equal, assert_greater_than, assert_raises
 TIMESTAMP_WINDOW = 2 * 60 * 60
 
 def mine_large_blocks(node, n):
-    txid=[]
+    txid=None
     # Make a large scriptPubKey for the coinbase transaction. This is OP_RETURN
     # followed by 950k of OP_NOP. This would be non-standard in a non-coinbase
     # transaction but is consensus valid.
