272282ca22751ae4b50b150de3d669bf39c51101
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-06-07 10:15:16 +0100

    Add disconnect_nodes in feature timestampindex

--- test/functional/feature_timestampindex.py
+++ test/functional/feature_timestampindex.py
@@ -9,7 +9,7 @@
 
 from test_framework.test_framework import WagerrTestFramework
 from test_framework.test_node import ErrorMatch
-from test_framework.util import assert_equal, connect_nodes
+from test_framework.util import assert_equal, connect_nodes, disconnect_nodes
 
 
 class TimestampIndexTest(WagerrTestFramework):
