3fc21f2f75b2d487ec383b7dc7a52a5130440729
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-06-07 09:59:38 +0100

    Add spork key in feature sporks

--- test/functional/feature_sporks.py
+++ test/functional/feature_sporks.py
@@ -13,7 +13,7 @@ class SporkTest(WagerrTestFramework):
     def set_test_params(self):
         self.num_nodes = 3
         self.setup_clean_chain = True
-        self.extra_args = [["-sporkkey=6xLZdACFRA53uyxz8gKDLcgVrm5kUUEu2B3BUzWUxHqa2W7irbH"], ["-sporkkey=6xLZdACFRA53uyxz8gKDLcgVrm5kUUEu2B3BUzWUxHqa2W7irbH"], []]
+        self.extra_args = [["-sporkkey=6xLZdACFRA53uyxz8gKDLcgVrm5kUUEu2B3BUzWUxHqa2W7irbH"], ["-sporkkey=6xLZdACFRA53uyxz8gKDLcgVrm5kUUEu2B3BUzWUxHqa2W7irbH"], ["-sporkkey=6xLZdACFRA53uyxz8gKDLcgVrm5kUUEu2B3BUzWUxHqa2W7irbH"]]
 
     def setup_network(self):
         self.disable_mocktime()
