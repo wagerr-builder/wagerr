fdc259e8d91cc969d6389c5ca4439f28e4e0fa1c
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-06-13 11:17:06 +0100

    Move breakpoint in p2p compactblocks

--- test/functional/p2p_compactblocks.py
+++ test/functional/p2p_compactblocks.py
@@ -117,12 +117,12 @@ class CompactBlocksTest(WagerrTestFramework):
     def make_utxos(self):
         # Doesn't matter which node we use, just use node0.
         block = self.build_block_on_tip(self.nodes[0])
-        breakpoint()
         serialized_block = hexlify(block.serialize()).decode()
         self.nodes[0].submitblock(serialized_block)
         #self.test_node.send_and_ping(msg_block(block))
         assert int(self.nodes[0].getbestblockhash(), 16) == block.sha256
         self.nodes[0].generate(100)
+        breakpoint()
 
         total_value = block.vtx[0].vout[0].nValue
         out_value = total_value // 10
