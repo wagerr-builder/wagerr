bd3b2f0c27a993e90f27460abc9f71f90dc44d33
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-06-15 14:24:04 +0100

    Remove self.test_compactblocks_not_at_tip( in p2p compactblocks 3

--- test/functional/p2p_compactblocks.py
+++ test/functional/p2p_compactblocks.py
@@ -769,7 +769,7 @@ class CompactBlocksTest(WagerrTestFramework):
         #self.test_compactblocks_not_at_tip(self.nodes[0], self.test_node)
         self.sync_blocks()
         #self.test_compactblocks_not_at_tip(self.nodes[1], self.second_node)
-        self.test_compactblocks_not_at_tip(self.nodes[1], self.old_node)
+        #self.test_compactblocks_not_at_tip(self.nodes[1], self.old_node)
         self.sync_blocks()
 
         self.log.info("Testing handling of incorrect blocktxn responses...")
