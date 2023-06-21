37a6d4551ece85b7ccbe83beb9b46698c3283437
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-06-21 11:11:12 +0100

    Remove breakpoint from test_framework

--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -1067,7 +1067,6 @@ class WagerrTestFramework(WagerrTestFramework):
         assert min_inputs <= max_inputs
         # fill inputs
         inputs = []
-        breakpoint()
         balances = node_from.listunspent()
         in_amount = 0.0
         last_amount = 0.0
