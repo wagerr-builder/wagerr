7ce128393f34b9a398330540aa052ae37227ea80
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2023-06-24 13:28:35 +0100

    Add cs_main lock to rpctokenswallet 5

--- src/tokens/rpctokenwallet.cpp
+++ src/tokens/rpctokenwallet.cpp
@@ -581,8 +581,8 @@ extern UniValue listtokenssinceblock(const JSONRPCRequest& request)
 
     pwallet->BlockUntilSyncedToCurrentChain();
 
-    LOCK(cs_main);
     //LOCK(pwallet->cs_wallet);
+    LOCK2(cs_main, pwallet->cs_wallet);
 
     unsigned int curparam = 0;
 
