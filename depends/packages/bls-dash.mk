b622e11245613d32a5391d89fc376b761ea4b921
wagerr-builder <104495875+wagerr-builder@users.noreply.github.com>
2024-04-05 10:18:40 +0100

    Add in libsodium to bls-dash extract 3

--- depends/packages/bls-dash.mk
+++ depends/packages/bls-dash.mk
@@ -18,7 +18,7 @@ $(package)_libsodium_version=1.0.18
 $(package)_libsodium_download_path=https://github.com/wagerr-builder/libsodium-cmake/releases/download/V$($(package)_libsodium_version)
 $(package)_libsodium_download_file=libsodium-cmake-$($(package)_libsodium_version).tar.gz
 $(package)_libsodium_file_name=$($(package)_libsodium_download_file)
-$(package)_libsodium_build_subdir=libsodium-cmake
+$(package)_libsodium_build_subdir=build/_deps/sodium-subbuild
 $(package)_libsodium_sha256_hash=13b8939f75bebd6ff0fac49548fbe1a4c2ac477444b2d68a7621e233339e0874
 
 $(package)_extra_sources=$($(package)_relic_file_name)
@@ -27,6 +27,7 @@ $(package)_extra_sources_libsodium=$($(package)_libsodium_file_name)
 define $(package)_fetch_cmds
 $(call fetch_file,$(package),$($(package)_download_path),$($(package)_download_file),$($(package)_file_name),$($(package)_sha256_hash)) && \
 $(call fetch_file,$(package),$($(package)_relic_download_path),$($(package)_relic_download_file),$($(package)_relic_file_name),$($(package)_relic_sha256_hash)) && \
+echo "$($(package)_libsodium_sha256_hash)  $($(package)_source_dir)/$($(package)_libsodium_file_name)" && \
 $(call fetch_file,$(package),$($(package)_libsodium_download_path),$($(package)_libsodium_download_file),$($(package)_libsodium_file_name),$($(package)_libsodium_sha256_hash))
 endef
 
